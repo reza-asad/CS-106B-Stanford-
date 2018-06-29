1a10d5799072ab123c2d7e557411f80c906a09de
Reza Asad <rezaasad@rezas-macbook-pro.local>
2018-06-29 10:53:44 -0700

    Initial Commit

---
+++ CS 106B Stanford/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS 106B Stanford
+//
+//  Created by Reza Asad on 6/29/18.
+//  Copyright © 2018 Reza Asad. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
