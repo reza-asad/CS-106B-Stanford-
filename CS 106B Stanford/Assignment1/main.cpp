3628211c3cb60ff781799f186d22435a34eef166
Reza Asad <rayza.asad@gmail.com>
2018-06-29 11:35:06 -0700

    starting assignemnt 1

--- CS 106B Stanford/Assignment1/main.cpp
+++ CS 106B Stanford/Assignment1/main.cpp
@@ -1,15 +1,17 @@
 //
 //  main.cpp
 //  CS 106B Stanford
+//  This is Assignemnt1
 //
 //  Created by Reza Asad on 6/29/18.
 //  Copyright © 2018 Reza Asad. All rights reserved.
 //
+//
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    cout << "Hello world" << endl;
     return 0;
 }
