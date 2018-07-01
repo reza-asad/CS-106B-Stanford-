7c40784335ea96146fa292f46d38fa0339083505
Reza Asad <rayza.asad@gmail.com>
2018-07-01 14:14:41 -0700

    starting assignment 2

---
+++ Assignment2/Assignment2/utility.cpp
@@ -0,0 +1,9 @@
+//
+//  utility.cpp
+//  Assignment2
+//
+//  Created by Reza Asad on 7/1/18.
+//  Copyright © 2018 Reza Asad. All rights reserved.
+//
+
+#include "utility.hpp"
---
+++ Assignment2/Assignment2/utility.hpp
@@ -0,0 +1,14 @@
+//
+//  utility.hpp
+//  Assignment2
+//
+//  Created by Reza Asad on 7/1/18.
+//  Copyright © 2018 Reza Asad. All rights reserved.
+//
+
+#ifndef utility_hpp
+#define utility_hpp
+
+#include <stdio.h>
+
+#endif /* utility_hpp */
