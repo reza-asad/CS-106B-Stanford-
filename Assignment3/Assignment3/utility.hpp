57074130d6665375a475e8f709628896c3788ca3
Reza Asad <rayza.asad@gmail.com>
2018-07-03 12:40:28 -0700

    changed the structure and renamed files for assignment 1

---
+++ Assignment3/Assignment3/utility.hpp
@@ -0,0 +1,14 @@
+//
+//  utility.hpp
+//  Assignment3
+//
+//  Created by Reza Asad on 7/3/18.
+//  Copyright © 2018 Reza Asad. All rights reserved.
+//
+
+#ifndef utility_hpp
+#define utility_hpp
+
+#include <stdio.h>
+
+#endif /* utility_hpp */
